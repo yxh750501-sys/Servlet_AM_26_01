d76489038b7be16d9b512598b518e5253ba2fe64
yoonjongmin <yxh750501@gmail.com>
2026-01-16 04:11:34 +0900

    서블릿 구구단 기초

--- src/main/java/com/KoreaIT/java/AM_jsp/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/HomeMainServlet2.java
@@ -8,12 +8,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main")
-public class HomeMainServlet extends HttpServlet {
+@WebServlet("/home/main2")
+public class HomeMainServlet2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("hello world").append(request.getContextPath());
 	}
 	
 }
--- src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
@@ -8,12 +8,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main")
-public class HomeMainServlet extends HttpServlet {
+@WebServlet("/printDan")
+public class PrintDanServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+		response.getWriter().append("==8단==<br>");
+		
+		int dan = 8;
+		
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
+		
 	}
 	
 }
