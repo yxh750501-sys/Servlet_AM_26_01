3e0838801d7946b6871ca0a9273129bbdba33549
yoonjongmin <yxh750501@gmail.com>
2026-01-16 03:42:14 +0900

    서블릿 생성

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/HomeMainServlet.java
@@ -0,0 +1,19 @@
+package com.KoreaIT.java.AM_jsp;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/HomeMainServlet")
+public class HomeMainServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+}
